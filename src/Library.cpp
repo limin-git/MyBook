eddc0343f20f714e231b02be6eb7798717cf1eaa
zhulimin <zhulimin.china@gmail.com>
2015-05-19 22:35:15 +0800

    refactor

--- src/Library.cpp
+++ src/Library.cpp
@@ -14,7 +14,344 @@ Library::Library( const path& p )
 }
 
 
-bool Library::is_parent_sub_path( const path& parent, const path& sub )
+void Library::add_book( const path& book_path, const std::string& folder_name )
+{
+    if ( ! boost::filesystem::exists( book_path ) )
+    {
+        std::cout << "找不到：" << book_path.string() << std::endl;
+        return;
+    }
+
+    if ( ! is_clc_folder_name( folder_name ) )
+    {
+        std::cout << "不符合图书馆分类法："<< folder_name << std::endl;
+        return;
+    }
+
+    create_index( folder_name[0] );
+
+    std::map<std::string, path>::iterator it = m_class_path_map.find( folder_name );
+
+    if ( it == m_class_path_map.end() )
+    {
+        if ( false == creaate_clc_folder( folder_name ) )
+        {
+            std::cout << "创建文件夹失败。" << std::endl;
+            return;
+        }
+
+        it = m_class_path_map.find( folder_name );
+    }
+
+    if ( it == m_class_path_map.end() )
+    {
+        std::cout << "找不到文件夹：" << folder_name << std::endl;
+        return;
+    }
+
+    {
+        std::map<std::string, path>::iterator find_it = m_file_path_map.find( book_path.filename().string() );
+
+        if ( find_it != m_file_path_map.end() && find_it->second.parent_path() != it->second  )
+        {
+            std::cout << "已存在，但路径不同：" << find_it->second.string() << std::endl;
+        }
+    }
+
+    path new_book_path = it->second / book_path.filename();
+
+    if ( boost::filesystem::exists( new_book_path ) )
+    {
+        std::cout << "已存在：" << new_book_path.string() << std::endl;
+
+        if ( is_equal_size( book_path, new_book_path ) )
+        {
+            std::cout << "大小相同，删除文件：" << book_path.string() << std::endl;
+            remove_path( book_path );
+            return;
+        }
+
+        for ( size_t i = 1; true ; ++i )
+        {
+            std::string post_fix = "(" + boost::lexical_cast<std::string>(i) + ")";
+            new_book_path = it->second / ( book_path.stem().string() + post_fix + book_path.extension().string() );
+
+            if ( ! boost::filesystem::exists( new_book_path ) )
+            {
+                std::cout << "大小不同，重命名为：" << new_book_path.filename().string() << std::endl;
+                break;
+            }
+            else
+            {
+                if ( is_equal_size( book_path, new_book_path ) )
+                {
+                    std::cout << "大小相同，删除文件：" << book_path.string() << std::endl;
+                    remove_path( book_path );
+                    return;
+                }
+            }
+        }
+    }
+
+    std::cout << "正在移动文件……";
+
+    boost::system::error_code ec;
+    boost::filesystem::rename( book_path, new_book_path );
+
+    if ( ec )
+    {
+        std::cout << "\r移动文件失败：" << ec.message() << std::endl;
+        return;
+    }
+
+    // update cache
+    m_file_path_map[book_path.filename().string()] = new_book_path;
+    m_file_count++;
+    std::cout << "\r成功：" << new_book_path.string() << std::endl;
+}
+
+
+void Library::add_directory( const path& dir )
+{
+    if ( ! boost::filesystem::exists( dir ) )
+    {
+        std::cout << "目录不存在：" << dir << std::endl;
+        return;
+    }
+
+    if ( ! boost::filesystem::is_directory( dir ) )
+    {
+        std::cout << "不是目录：" << dir << std::endl;
+        return;
+    }
+
+    if ( ! is_clc_folder_name( boost::to_upper_copy( dir.filename().string() ) ) )
+    {
+        std::cout << "不符合图书馆分类法：" << dir << std::endl;
+        return;
+    }
+
+    boost::filesystem::directory_iterator end; // default construction yields past-the-end
+    boost::filesystem::directory_iterator it( dir );
+
+    for ( ; it != end; ++it )
+    {
+        const path& p = *it;
+
+        if ( true == is_directory( it->status() ) )
+        {
+            add_directory( p );
+        }
+        else
+        {
+            add_book( p, boost::to_upper_copy( p.parent_path().filename().string() ) );
+        }
+    }
+
+    remove_path( dir );
+}
+
+
+bool Library::creaate_clc_folder( const std::string& folder_name )
+{
+    std::map<std::string, path>::iterator it = m_class_path_map.find( folder_name );
+
+    if ( it != m_class_path_map.end() )
+    {
+        return true;
+    }
+
+    std::cout << "创建新文件夹：" << folder_name << std::endl;
+
+    path parent;
+    path base_name;
+    std::string sub_dirs;
+    size_t size = folder_name.size();
+
+    for ( size_t i = 0; i < size; ++i )
+    {
+        std::string n = folder_name.substr( 0, size - i );
+        it = m_class_path_map.find( n );
+
+        if ( it != m_class_path_map.end() )
+        {
+            sub_dirs = folder_name.substr( size - i );
+            parent = it->second;
+            base_name = it->first;
+            break;
+        }
+    }
+
+    if ( sub_dirs.empty() || parent.empty() )
+    {
+        std::cout << "失败，找不到父文件夹。" << std::endl;
+        return false;
+    }
+
+    if ( ! boost::filesystem::exists( parent ) )
+    {
+        std::cout << "失败，找不到文件夹：" << parent.string() << std::endl;
+        return false;
+    }
+
+    path new_parent = parent;
+    //std::cout << base_name.string() << std::endl;
+
+    for ( size_t i = 0; i < sub_dirs.size(); ++i )
+    {
+        if ( sub_dirs[i] == '.' )
+        {
+            continue;
+        }
+
+        if ( sub_dirs[i] == '-' || '=' == sub_dirs[i] )
+        {
+            i = sub_dirs.size();
+        }
+
+        path new_name = path( base_name.string() + sub_dirs.substr(0, i + 1) );
+        path new_path = new_parent / new_name;
+        std::map<std::string, path>::iterator find_it = m_class_path_map.find( new_name.string() );
+
+        if ( find_it != m_class_path_map.end() )
+        {
+            std::cout << "成功，但新文件夹已存在： " << new_name.string() << std::endl;
+            return true;
+        }
+
+        if ( false == boost::filesystem::create_directory( new_path ) )
+        {
+            std::cout << "失败，系统错误：" << new_path.string() << std::endl;
+            return false;
+        }
+
+        m_class_path_map[new_name.string()] = new_path;
+        new_parent = new_path;
+    }
+
+    return true;
+}
+
+
+void Library::create_index( const char top_class_name )
+{
+    if ( false == m_path_exist )
+    {
+        return;
+    }
+
+    if ( top_class_name < m_current_top_class )
+    {
+        return;
+    }
+
+    for ( ; m_it != m_end; ++m_it )
+    {
+        if ( false == is_directory( m_it->status() ) )
+        {
+            std::string filename = m_it->path().filename().string();
+            std::map<std::string, path>::iterator find_it = m_file_path_map.find( filename );
+
+            if ( find_it == m_file_path_map.end() )
+            {
+                m_file_path_map[filename] = *m_it;
+                m_file_count++;
+            }
+
+            continue;
+        }
+
+        const path& p = *m_it;
+        const std::string s = p.filename().string();
+        boost::smatch m;
+
+        if ( boost::regex_search( s, m, m_regex ) )
+        {
+            if ( false == is_clc_directory(p) )
+            {
+                // std::cout << p.string() << std::endl;
+                continue;
+            }
+
+            //std::cout << p.string() << std::endl;
+            //std::cout << p.filename().string() << std::endl;
+            std::string class_name = m.str();
+            std::map<std::string, path>::iterator find_it = m_class_path_map.find( class_name );
+
+            if ( find_it == m_class_path_map.end())
+            {
+                m_class_path_map[class_name] = p;
+                m_folder_count++;
+            }
+            else
+            {
+                if ( find_it->second.filename().string().find( "依世界" ) != std::string::npos && is_parent_path( find_it->second, p ) )
+                {
+                    m_class_path_map[class_name] = p;
+                }
+                else
+                {
+                    std::cout << "文件夹名相同：" << p.string() << " <<==>> " << find_it->second.string() << std::endl;
+                }
+            }
+
+            if ( m_current_top_class != class_name[0] )
+            {
+                std::cout << "索引：" << m_current_top_class << std::endl;
+                m_current_top_class = class_name[0];
+
+                if ( top_class_name < m_current_top_class )
+                {
+                    m_it++;
+                    return;
+                }
+            }
+        }
+    }
+
+    if ( m_it == m_end )
+    {
+        m_current_top_class++;
+        std::cout << "索引完成，共 " << m_file_count << " 个文件，" << m_folder_count << " 个文件夹。" << std::endl;
+    }
+}
+
+
+bool Library::is_equal_size( const path& lhs, const path& rhs )
+{
+    boost::system::error_code ec1, ec2;
+    boost::uintmax_t size_1 = boost::filesystem::file_size( lhs, ec1 );
+    boost::uintmax_t size_2 = boost::filesystem::file_size( rhs, ec2 );
+
+    if ( ec1 )
+    {
+        std::cout << "不能获取文件大小：" << lhs << ", 错误码：" << ec1.message() << std::endl;
+        return false;
+    }
+
+    if ( ec2 )
+    {
+        std::cout << "不能获取文件大小：" << rhs << ", 错误码：" << ec2.message() << std::endl;
+        return false;
+    }
+
+    return size_1 == size_2;
+}
+
+
+void Library::remove_path( const path& book_path )
+{
+    boost::system::error_code ec;
+    boost::filesystem::remove( book_path );
+
+    if ( ec )
+    {
+        std::cout << "删除：" << book_path.string() << " 失败，错误码：" << ec.message() << std::endl;
+    }
+}
+
+
+bool Library::is_parent_path( const path& parent, const path& sub )
 {
     if ( false == sub.has_parent_path() )
     {
@@ -114,332 +451,4 @@ bool Library::is_book_exist( const path& book_path, bool is_output  )
     }
 
     return false;
-}
-
-
-bool Library::creaate_clc_folder( const std::string& folder_name )
-{
-    std::map<std::string, path>::iterator it = m_class_path_map.find( folder_name );
-
-    if ( it != m_class_path_map.end() )
-    {
-        return true;
-    }
-
-    std::cout << "创建新文件夹：" << folder_name << std::endl;
-
-    path parent;
-    path base_name;
-    std::string sub_dirs;
-    size_t size = folder_name.size();
-
-    for ( size_t i = 0; i < size; ++i )
-    {
-        std::string n = folder_name.substr( 0, size - i );
-        it = m_class_path_map.find( n );
-
-        if ( it != m_class_path_map.end() )
-        {
-            sub_dirs = folder_name.substr( size - i );
-            parent = it->second;
-            base_name = it->first;
-            break;
-        }
-    }
-
-    if ( sub_dirs.empty() || parent.empty() )
-    {
-        std::cout << "失败，找不到父文件夹。" << std::endl;
-        return false;
-    }
-
-    if ( ! boost::filesystem::exists( parent ) )
-    {
-        std::cout << "失败，找不到文件夹：" << parent.string() << std::endl;
-        return false;
-    }
-
-    path new_parent = parent;
-    //std::cout << base_name.string() << std::endl;
-
-    for ( size_t i = 0; i < sub_dirs.size(); ++i )
-    {
-        if ( sub_dirs[i] == '.' )
-        {
-            continue;
-        }
-
-        if ( sub_dirs[i] == '-' || '=' == sub_dirs[i] )
-        {
-            i = sub_dirs.size();
-        }
-
-        path new_name = path( base_name.string() + sub_dirs.substr(0, i + 1) );
-        path new_path = new_parent / new_name;
-        std::map<std::string, path>::iterator find_it = m_class_path_map.find( new_name.string() );
-
-        if ( find_it != m_class_path_map.end() )
-        {
-            std::cout << "成功，但新文件夹已存在： " << new_name.string() << std::endl;
-            return true;
-        }
-
-        if ( false == boost::filesystem::create_directory( new_path ) )
-        {
-            std::cout << "失败，系统错误：" << new_path.string() << std::endl;
-            return false;
-        }
-
-        m_class_path_map[new_name.string()] = new_path;
-        new_parent = new_path;
-    }
-
-    return true;
-}
-
-
-void Library::add_book( const path& book_path, const std::string& folder_name )
-{
-    if ( ! boost::filesystem::exists( book_path ) )
-    {
-        std::cout << "找不到：" << book_path.string() << std::endl;
-        return;
-    }
-
-    if ( ! boost::regex_match( folder_name, m_regex ) )
-    {
-        std::cout << "不符合图书馆分类法："<< folder_name << std::endl;
-        return;
-    }
-
-    create_index( folder_name[0] );
-
-    std::map<std::string, path>::iterator it = m_class_path_map.find( folder_name );
-
-    if ( it == m_class_path_map.end() )
-    {
-        if ( false == creaate_clc_folder( folder_name ) )
-        {
-            std::cout << "创建文件夹失败，不能添加新书：" << book_path.string() << std::endl;
-            return;
-        }
-
-        it = m_class_path_map.find( folder_name );
-    }
-
-    if ( it == m_class_path_map.end() )
-    {
-        std::cout << "找不到文件夹：" << folder_name << std::endl;
-        return;
-    }
-
-    path new_book_path = it->second / book_path.filename();
-
-    if ( boost::filesystem::exists( new_book_path ) )
-    {
-        std::cout << "已存在：" << new_book_path.string() << std::endl;
-
-        if ( is_size_equal( book_path, new_book_path ) )
-        {
-            std::cout << "文件大小相同，删除文件：" << book_path.string() << std::endl;
-            remove_book( book_path );
-            return;
-        }
-
-        for ( size_t i = 1; i < 100; ++i )
-        {
-            std::string post_fix = "(" + boost::lexical_cast<std::string>(i) + ")";
-            new_book_path = it->second / ( book_path.stem().string() + post_fix + book_path.extension().string() );
-
-            if ( ! boost::filesystem::exists( new_book_path ) )
-            {
-                std::cout << "文件大小不同，重命名为：" << new_book_path.filename().string() << std::endl;
-                break;
-            }
-            else
-            {
-                if ( is_size_equal( book_path, new_book_path ) )
-                {
-                    std::cout << "文件大小相同，删除文件：" << book_path.string() << std::endl;
-                    remove_book( book_path );
-                    return;
-                }
-            }
-        }
-    }
-
-    std::cout << "正在移动文件……";
-
-    boost::system::error_code ec;
-    boost::filesystem::rename( book_path, new_book_path );
-
-    if ( ec )
-    {
-        std::cout << "\r移动文件失败：" << ec.message() << std::endl;
-        return;
-    }
-
-    // update cache
-    m_file_path_map[book_path.filename().string()] = new_book_path;
-    m_file_count++;
-    std::cout << "\r成功：" << new_book_path.string() << std::endl;
-}
-
-
-void Library::add_directory( const path& dir )
-{
-    if ( ! boost::filesystem::exists( dir ) )
-    {
-        std::cout << "文件夹不存在：" << dir << std::endl;
-        return;
-    }
-
-    if ( ! boost::filesystem::is_directory( dir ) )
-    {
-        std::cout << "不是文件夹：" << dir << std::endl;
-        return;
-    }
-
-    if ( ! is_clc_folder_name( boost::to_upper_copy( dir.filename().string() ) ) )
-    {
-        std::cout << "不符合图书馆分类法：" << dir << std::endl;
-        return;
-    }
-
-    boost::filesystem::directory_iterator end; // default construction yields past-the-end
-    boost::filesystem::directory_iterator it( dir );
-
-    for ( ; it != end; ++it )
-    {
-        const path& p = *it;
-
-        if ( true == is_directory( it->status() ) )
-        {
-            add_directory( p );
-        }
-        else
-        {
-            add_book( p, boost::to_upper_copy( p.parent_path().filename().string() ) );
-        }
-    }
-
-    boost::filesystem::remove( dir );
-}
-
-
-void Library::create_index( const char top_class_name )
-{
-    if ( false == m_path_exist )
-    {
-        return;
-    }
-
-    if ( top_class_name < m_current_top_class )
-    {
-        return;
-    }
-
-    for ( ; m_it != m_end; ++m_it )
-    {
-        if ( false == is_directory( m_it->status() ) )
-        {
-            std::string filename = m_it->path().filename().string();
-            std::map<std::string, path>::iterator find_it = m_file_path_map.find( filename );
-
-            if ( find_it == m_file_path_map.end() )
-            {
-                m_file_path_map[filename] = *m_it;
-                m_file_count++;
-            }
-
-            continue;
-        }
-
-        const path& p = *m_it;
-        const std::string s = p.filename().string();
-        boost::smatch m;
-
-        if ( boost::regex_search( s, m, m_regex ) )
-        {
-            if ( false == is_clc_directory(p) )
-            {
-                //std::cout << check.string() << std::endl;
-                continue;
-            }
-
-            //std::cout << p.string() << std::endl;
-            //std::cout << p.filename().string() << std::endl;
-            std::string class_name = m.str();
-            std::map<std::string, path>::iterator find_it = m_class_path_map.find( class_name );
-
-            if ( find_it == m_class_path_map.end())
-            {
-                m_class_path_map[class_name] = p;
-                m_folder_count++;
-            }
-            else
-            {
-                if ( find_it->second.filename().string().find( "依世界" ) != std::string::npos && is_parent_sub_path( find_it->second, p ) )
-                {
-                    m_class_path_map[class_name] = p;
-                }
-                else
-                {
-                    std::cout << "文件夹名相同：" << p.string() << " <<==>> " << find_it->second.string() << std::endl;
-                }
-            }
-
-            if ( m_current_top_class != class_name[0] )
-            {
-                std::cout << "索引：" << m_current_top_class << std::endl;
-                m_current_top_class = class_name[0];
-
-                if ( top_class_name < m_current_top_class )
-                {
-                    m_it++;
-                    return;
-                }
-            }
-        }
-    }
-
-    if ( m_it == m_end )
-    {
-        m_current_top_class = 'z';
-        std::cout << "索引完成，共 " << m_file_count << " 个文件，" << m_folder_count << " 个文件夹。" << std::endl;
-    }
-}
-
-
-bool Library::is_size_equal( const path& lhs, const path& rhs )
-{
-    boost::system::error_code ec1, ec2;
-    boost::uintmax_t size_1 = boost::filesystem::file_size( lhs, ec1 );
-    boost::uintmax_t size_2 = boost::filesystem::file_size( rhs, ec2 );
-
-    if ( ec1 )
-    {
-        std::cout << "不能获取文件大小：" << lhs << ", 错误码：" << ec1.message() << std::endl;
-        return false;
-    }
-
-    if ( ec2 )
-    {
-        std::cout << "不能获取文件大小：" << rhs << ", 错误码：" << ec2.message() << std::endl;
-        return false;
-    }
-
-    return size_1 == size_2;
-}
-
-
-void Library::remove_book( const path& book_path )
-{
-    boost::system::error_code ec;
-    boost::filesystem::remove( book_path );
-
-    if ( ec )
-    {
-        std::cout << "删除文件失败：" << ec.message() << std::endl;
-    }
 }
--- src/Library.h
+++ src/Library.h
@@ -7,21 +7,20 @@ class Library
 public:
 
     Library( const path& p );
-
-    bool creaate_clc_folder( const std::string& folder_name );
     void add_book( const path& book_path, const std::string& folder_name );
     void add_directory( const path& folder );
 
 public:
 
-    bool is_parent_sub_path( const path& parent, const path& sub );
+    bool creaate_clc_folder( const std::string& folder_name );
+    void create_index( const char top_class_name );
+    void remove_path( const path& book_path );
+    bool is_parent_path( const path& parent, const path& p );
     bool is_clc_folder_name( const std::string& folder_name );
     bool is_clc_directory( const path& dir );
     bool is_valid_clc_directory( const path& p );
     bool is_book_exist( const path& file_path, bool is_output = false );
-    void create_index( const char top_class_name );
-    bool is_size_equal( const path& lhs, const path& rhs );
-    void remove_book( const path& book_path );
+    bool is_equal_size( const path& lhs, const path& rhs );
 
 public:
 
